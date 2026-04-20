57f659b7333518571b0fec3b0fea63b0d826d342
Himanshu Raj <himanshuraj650@gmail.com>
2026-04-20 23:00:19 +0530

    Add starter React app source

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return (
+    <main style={{padding:'2rem',fontFamily:'sans-serif'}}>
+      <h1>RetroVision</h1>
+      <p>Project pushed from provided archive.</p>
+    </main>
+  )
+}
